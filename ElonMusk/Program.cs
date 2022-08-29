95114e6be8caa5d4cca576ce7b755c721094900f
zbcevlo <zbcevlo@ZBC-E-RO-23262>
2022-08-29 16:24:09 +0200

    Add project files.

---
+++ ElonMusk/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace ElonMusk
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an array with the possible colors of the car, and puts them into a random
+            string[] colour = { "Red", "Yellow", "Green" };
+
+            Random random = new Random();
+
+            string color = colour[random.Next(colour.Length)];
+
+            // Making a new instance of the object
+            Tesla t = new Tesla(color);
+
+            // Recieving a valuable for t.distance
+            Console.WriteLine("How many meters to drive");
+            t.Distance = Int32.Parse(Console.ReadLine());
+
+            // Runs the drive method
+
+            t.Drive();
+
+            // Writes out the color of the car, meters driven and % battery left
+            Console.WriteLine("The car is {0}", t.Colour);
+
+            Console.WriteLine("You have driven {0} meters", t.Distance);
+            Console.WriteLine("You have {0}% battery left",t.Battery);
+
+            // Calls the method recharge, that recharge the car and shows battery %
+            t.Recharge();
+
+            Console.WriteLine("You have {0}% battery left", t.Battery);
+
+            // Runs drive though again to show, we have used the recharged battery power
+            Console.WriteLine("How many meters to drive");
+            t.Distance = Int32.Parse(Console.ReadLine());
+
+            t.Drive();
+
+            
+
+            Console.WriteLine("You have driven {0} meters", t.Distance);
+            Console.WriteLine("You have {0}% battery left", t.Battery);
+
+            
+
+            Console.ReadKey();
+
+           
+
+        }
+    }
+}
---
+++ ElonMusk/Tesla.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElonMusk
+{
+    internal class Tesla
+    {
+        // private valuables
+        private string colour;
+        private int battery;
+        private int distance;
+
+        // Property for Color
+        public string Colour
+        {
+            get { return colour; }
+            set { colour = value; }
+        }
+
+        // Property for Battery
+        public int Battery
+        {
+            get { return battery; }
+            set { battery = value; }
+        }
+
+        // Property for Distance
+        public int Distance
+        {
+            get { return distance; }
+            set { distance = value; }
+        }
+
+        // Constructor recieveing 1 parameter
+        public Tesla(string color)
+        {
+            colour = color;
+            battery = 100;
+            distance = 0;
+
+        }
+
+        // Drive method
+        public void Drive()
+        {
+            for (int i = 0; i < Distance; i+=20)
+            {
+                --battery;
+            }
+
+
+        }
+
+        // Recharge method
+        public void Recharge()
+        {
+            battery = 100;
+            Distance = 0;
+        }
+
+    }
+}
